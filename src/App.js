20be033ca706e80002fafaf9ab59de8ba37bccbb
Marc Kuo <kuomarc2@gmail.com>
2020-01-03 14:02:48 -0800

    test lambda dependencies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,43 @@
 import React, { Component } from "react"
-import logo from "./logo.svg"
+import { Input, Row, Col, Layout, Table } from 'antd';
 import "./App.css"
+const { Search } = Input;
+const { Header, Content } = Layout;
+
+
+const dataSource = [
+  {
+    key: '1',
+    name: 'Mike',
+    age: 32,
+    address: '10 Downing Street',
+  },
+  {
+    key: '2',
+    name: 'John',
+    age: 42,
+    address: '10 Downing Street',
+  },
+];
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+];
+
 
 class LambdaDemo extends Component {
   constructor(props) {
@@ -35,16 +72,44 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+      <Layout>
+        <Header>
+          <Row>
+            <Col span={12}>
+              <Search
+                placeholder="Which search term would you like to analyze?"
+                onSearch={value => handleSearch(value)}
+                size="large"/>
+            </Col>
+          </Row>
+        </Header>
+        <Content>
+          <Row>
+            <Col span={6} offset={1}>
+              <Table dataSource={dataSource} columns={columns} />
+            </Col>
+            <Col span={6} offset={1}>
+              <Table dataSource={dataSource} columns={columns} />
+            </Col>
+          </Row>
           <LambdaDemo />
-        </header>
+        </Content>
+      </Layout>
       </div>
     )
   }
 }
 
 export default App
+
+function handleSearch(keywords) {
+  console.log('searching with: ' + keywords);
+  fetch("/.netlify/functions/scrape", {
+    method: 'POST',
+    body: 'test'
+  })
+    .then((err, res) => {
+      console.log(err);
+      console.log(res);
+    });
+}
--- src/lambda/async-dadjoke.js
+++ src/lambda/async-dadjoke.js
@@ -1,9 +1,19 @@
 // example of async handler using async-await
 // https://github.com/netlify/netlify-lambda/issues/43#issuecomment-444618311
 
-import axios from "axios"
+import axios from "axios";
+import Xray from 'x-ray';
+import serp from 'serp';
+import NGrams from 'word-ngrams';
+const x = Xray();
+
 export async function handler(event, context) {
   try {
+    var corpus = [];
+    const links = await serp.search(options);
+    console.log(links);
+
+
     const response = await axios.get("https://icanhazdadjoke.com", { headers: { Accept: "application/json" } })
     const data = response.data
     return {
